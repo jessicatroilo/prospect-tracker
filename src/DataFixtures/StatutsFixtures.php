5bb263cb364b57cc3823bd89be1681c2967bab76
Jessica <jessica.troilo25@gmail.com>
2026-03-10 09:38:42 +0100

    add fixture for Statuts

---
+++ src/DataFixtures/StatutsFixtures.php
@@ -0,0 +1,30 @@
+<?php 
+
+namespace App\DataFixtures;
+
+use App\Entity\Statuts;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class StatutsFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $statuts = [
+            'Identifié',
+            'Approché',
+            'A relancer',
+            'Rendez-vous fixé',
+            'Proposition envoyée',
+            'Signé'
+        ];
+
+        foreach ($statuts as $label) {
+            $statut = new Statuts();
+            $statut->setPrincipal($label);
+            $manager->persist($statut);
+        }
+
+        $manager->flush();
+    }
+}
